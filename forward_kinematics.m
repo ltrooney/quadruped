bc1159b5455566c94697608bd7efda822e7cd3b8
Luke Rooney <lukerooney@Lukes-iMac.local>
2019-09-08 11:37:23 -0700

    fixed error in transformation matrices T01 and T12

--- forward_kinematics.m
+++ forward_kinematics.m
@@ -7,9 +7,9 @@ l = 1;  % length %
 w = 0.4; % width %
 
 position = [0 0 0]; % x, y, z %
-orientation = [0 0 0];  % roll, pitch, yaw %
+orientation = [0 0 0];  % roll, yaw, pitch %
 
-theta = [0 0 0];    % theta_1, theta_2, theta_3 %
+theta = [0 -pi/6 pi/4];    % theta_1, theta_2, theta_3 %
 
 % construct XYZ rotation matrix %
 Rx = [1 0 0 0; 
@@ -38,10 +38,10 @@ Tlb = Tm * [0 0 -1 -l/2; 0 1 0 0; 1 0 0 -w/2; 0 0 0 1];
 
 T01 = [cos(theta(1)) -sin(theta(1)) 0 -L1*cos(theta(1));
         sin(theta(1)) cos(theta(1)) 0 -L1*sin(theta(1));
-        1 0 0 0;
+        0 0 1 0;
         0 0 0 1];
 
-T12 = [0 0 -1 0; -1 0 0 0; 0 0 1 0; 0 0 0 1];
+T12 = [0 0 -1 0; -1 0 0 0; 0 1 0 0; 0 0 0 1];
 
 T23 = [cos(theta(2)) -sin(theta(2)) 0 L2*cos(theta(2));
         sin(theta(2)) cos(theta(2)) 0 L2*sin(theta(2));
@@ -53,10 +53,74 @@ T34 = [cos(theta(3)) -sin(theta(3)) 0 L3*cos(theta(3));
         0 0 0 1];
 
 % transformation from base of leg to foot %
-T = T01 * T12 * T23 * T34;    
+T02 = T01 * T12;
+T03 = T02 * T23;
+T04 = T03 * T34;    
 
-disp(T * [0; 0; 0; 1])
+ee_vec = [0;0;0;1];
 
+body_coords = Tm*ee_vec;
+l20 = Trf * ee_vec
+l30 = Tlf * ee_vec;
+l10 = Trb * ee_vec;
+l40 = Tlb * ee_vec;
+
+l21 = Trf * T01 * ee_vec
+l22 = Trf * T02 * ee_vec
+l23 = Trf * T03 * ee_vec
+l24 = Trf * T04 * ee_vec
+
+hold on
+plotSide(l20);
+plotSide(l21);
+plotSide(l22);
+plotSide(l23);
+plotSide(l24);
+axis([-0.5 1.5 -1 0.2])
+xlabel("X")
+ylabel("Y")
+
+% hold on
+% plotTop(body_coords);
+% plotTop(l20);
+% plotTop(l30);
+% plotTop(l10);
+% plotTop(l40);
+% axis([-2 2 -2 2])
+% xlabel("Z")
+% ylabel("X")
+% hold off
+
+% hold on
+% plotSide(body_coords);
+% plotSide(l20);
+% plotSide(l30);
+% plotSide(l10);
+% plotSide(l40);
+% plotSide(l21);
+% plotSide(l22);
+% plotSide(l23);
+% plotSide(l24);
+% axis([-2 2 -2 2])
+% xlabel("X")
+% ylabel("Y")
+% hold off
+
+
+
+
+
+function plotTop(coords)
+    scatter(coords(3), coords(1));
+end
+
+function plotSide(coords)
+    scatter(coords(1), coords(2));
+end
+
+function plot3vec(coords)
+    scatter(coords(1), coords(2), coords(3));
+end
 
     
     
